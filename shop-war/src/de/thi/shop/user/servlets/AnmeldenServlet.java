8aab85bfc0bc32e48c922a8dd24a59be79dcb095
seyitarar <sea5270@thi.de>
2020-05-18 15:20:19 +0200

    AnmeldenServlet Get und Post update

--- shop-war/src/de/thi/shop/user/servlets/AnmeldenServlet.java
+++ shop-war/src/de/thi/shop/user/servlets/AnmeldenServlet.java
@@ -41,7 +41,7 @@ public class AnmeldenServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		request.setCharacterEncoding("UTF-8");
 		
@@ -81,13 +81,6 @@ public class AnmeldenServlet extends HttpServlet {
 		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 	
 
 }
