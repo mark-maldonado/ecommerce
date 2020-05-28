fc2c27806c806bae0e19bd3f40ecd2e9ae8f8abb
Mark Maldonado <maldonado_mark@hotmail.com>
2020-05-28 19:01:45 +0200

    KategorieBean: serailVersion hinzugefügt

--- shop-war/src/de/thi/shop/kategorie/beans/KategorieBean.java
+++ shop-war/src/de/thi/shop/kategorie/beans/KategorieBean.java
@@ -5,7 +5,8 @@ package de.thi.shop.kategorie.beans;
 import java.io.Serializable;
 
 public class KategorieBean implements Serializable {
-
+	private static final long serialVersionUID = 1L;
+	
 	private Long id;
 	private String kategorieName;
 	private byte[] bild;
