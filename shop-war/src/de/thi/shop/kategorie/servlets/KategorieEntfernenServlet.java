59dee173b63760cec9304c84a02d2cb198a6c618
Mark Maldonado <maldonado_mark@hotmail.com>
2020-05-28 18:14:24 +0200

    KategorieEntfernenServlet: imports aktualisiert

--- shop-war/src/de/thi/shop/kategorie/servlets/KategorieEntfernenServlet.java
+++ shop-war/src/de/thi/shop/kategorie/servlets/KategorieEntfernenServlet.java
@@ -3,7 +3,6 @@ package de.thi.shop.kategorie.servlets;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
