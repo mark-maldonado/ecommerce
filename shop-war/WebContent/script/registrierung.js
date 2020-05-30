e80c669ef0469e1cbf381bf7ffee7481bb0f554f
Mark Maldonado <maldonado_mark@hotmail.com>
2020-05-30 15:49:36 +0200

    formular, registrierung, registrierung.js: Formular Prüfung mit JS

--- shop-war/WebContent/script/registrierung.js
+++ shop-war/WebContent/script/registrierung.js
@@ -1,1 +1,32 @@
+// CREATED BY MARK MALDONADO //
+"use sctrict";
 
+// Wenn DOM Elemente geladen sind //
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+	var form = document.getElementById("form_registrierung");
+	form.addEventListener("submit", checkForm);
+	form.addEventListener("reset", confirmReset);
+}
+
+// Check Passwort Wiederholen //
+function checkForm(evt) {
+	var pw1 = document.getElementById("passwort_id");
+	var pw2 = document.getElementById("repeat_passwort_id");
+	var fehlermeldung = document.getElementById("fehlermeldung");
+	
+	if(pw1.value != pw2.value) {
+		fehlermeldung.innerHTML = "Bitte überprüfen Sie die Eingabe 'Passwort wiederholen'";
+		pw2.classList.add("fehlerinput");
+		evt.preventDefault();
+	}
+}
+
+// Reset bestätigen //
+function confirmReset(evt) {
+	var reallyReset = confirm("Formular wirklich zurücksetzen?");
+	if(!reallyReset) {
+		evt.preventDefault();
+	}
+}
