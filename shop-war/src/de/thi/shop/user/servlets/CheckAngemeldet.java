a7785e307c30c443c76cbc0fab45338dba9176fb
Mark Maldonado <maldonado_mark@hotmail.com>
2020-05-30 11:21:01 +0200

    CheckAngemeldet: import aktualisiert

--- shop-war/src/de/thi/shop/user/servlets/CheckAngemeldet.java
+++ shop-war/src/de/thi/shop/user/servlets/CheckAngemeldet.java
@@ -2,7 +2,6 @@ package de.thi.shop.user.servlets;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
