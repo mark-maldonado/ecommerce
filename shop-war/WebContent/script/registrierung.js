03add18399973e7d332ad838500a1e0a813b68a9
Mark Maldonado <maldonado_mark@hotmail.com>
2020-06-23 18:48:02 +0200

    registrierung.js: prüfen auf Passwort 6 Zeichen und 1 Zahl

--- shop-war/WebContent/script/registrierung.js
+++ shop-war/WebContent/script/registrierung.js
@@ -12,10 +12,19 @@ function init() {
 
 // Check Passwort Wiederholen //
 function checkForm(evt) {
+	// EingabeElemente zwischenspeichern
 	var pw1 = document.getElementById("passwort_id");
 	var pw2 = document.getElementById("repeat_passwort_id");
 	var fehlermeldung = document.getElementById("fehlermeldung");
 	
+	// Prüfen ob Passwort den Anforderungen entspricht
+	if(pw1.value.length < 6 || !/\d/.test(pw1.value)) {
+		fehlermeldung.innerHTML = "Bitte benutzen Sie ein Passwort, das 6 Zeichen Lang und mindestens eine Zahl beinhalten";
+		pw1.classList.add("fehlerinput");
+		evt.preventDefault();
+	}
+	
+	// Prüfen ob Passwort richtig wiederholt worden ist
 	if(pw1.value != pw2.value) {
 		fehlermeldung.innerHTML = "Bitte überprüfen Sie die Eingabe 'Passwort wiederholen'";
 		pw2.classList.add("fehlerinput");
