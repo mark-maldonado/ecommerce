120072c1f4d9fca7ee7aedd863a56dc7edaa381d
Mark Maldonado <maldonado_mark@hotmail.com>
2020-07-05 15:12:57 +0200

    ArtikelAktualisierenServlet: artikelBildName richtig lesen und
    nicht lesen wenn leer

--- shop-war/src/de/thi/shop/artikel/servlets/ArtikelAktualisierenServlet.java
+++ shop-war/src/de/thi/shop/artikel/servlets/ArtikelAktualisierenServlet.java
@@ -62,37 +62,6 @@ public class ArtikelAktualisierenServlet extends HttpServlet {
 		artikelBean.setPreis(Math.round(100f*Float.parseFloat(request.getParameter("artikelPreis"))));
 		artikelBean.setId(Long.parseLong(request.getParameter("artikelId")));
 		
-		//Handling f�r das Bild, das hochgeladen wurde
-		InputStream inputStream = null;
-		Part filePart = request.getPart("artikelBild");
-		inputStream = filePart.getInputStream();
-		
-		//Name von Bild bekommen und trimmen, weil ganzer Path wiedergegeben wird
-		String artikelBildName = filePart.getSubmittedFileName();
-		String trimmed = artikelBildName.substring(artikelBildName.lastIndexOf("\\") + 1).trim();
-		artikelBean.setBildName(trimmed);
-			
-		//Wenn nichts hochgeladen, wird das aktuelle Bild von der Datenbank geholt
-		InputStream original = null;
-		try(Connection con = ds.getConnection();
-				PreparedStatement pstmt = con.prepareStatement("SELECT bild FROM artikel WHERE id = ?")){
-			
-			pstmt.setLong(1, artikelBean.getId());
-			
-				try(ResultSet rs = pstmt.executeQuery()) {
-					if(rs.next()) {
-						original = rs.getBinaryStream("bild");
-					}
-				}
-		} catch (Exception ex) {
-			throw new ServletException(ex.getMessage());
-			}
-		
-		InputStreamReader reader = new InputStreamReader(inputStream);
-			if(!(reader.ready())){
-				inputStream = original;
-		}
-	
 		//KategorieId wird von der Datenbank geholt, da nur KategorieName von jsp weitergegeben wird
 		try(Connection con = ds.getConnection();
 				PreparedStatement pstmt = con.prepareStatement("SELECT * FROM kategorie WHERE name = ?")) {
@@ -106,24 +75,80 @@ public class ArtikelAktualisierenServlet extends HttpServlet {
 		} catch (Exception ex) {
 			throw new ServletException(ex.getMessage());
 		}
-	
-		//Update in der Datenbank
-			try(Connection con = ds.getConnection();
-					PreparedStatement pstmt = con.prepareStatement("UPDATE artikel SET name = ?, preis = ?, idKategorie = ?, bild = ?, bildname = ? WHERE id = ?")){
+		
+		//Test ob artikelBild mitgeliefert wurde
+		boolean bildAvailable = false;
+		try {
+			//Fehler bei nicht vorhandenem Bildnamen provozieren
+			request.getPart("artikelBild").getSubmittedFileName().substring(0, request.getPart("artikelBild").getSubmittedFileName().lastIndexOf("."));
+			bildAvailable = true;
+		} catch(Exception e) {
+			bildAvailable = false;
+		}
+		
+		
+		//Falls Bild ausgewaehlt wurde
+		if (bildAvailable) {
+			//Handling f�r das Bild, das hochgeladen wurde
+			InputStream inputStream = null;
+			Part filePart = request.getPart("artikelBild");
+			inputStream = filePart.getInputStream();
+			
+			//Name von Bild bekommen und trimmen, weil ganzer Path wiedergegeben wird
+			String artikelBildName = filePart.getSubmittedFileName();
+			String trimmed = artikelBildName.substring(0, artikelBildName.lastIndexOf("."));
+			artikelBean.setBildName(trimmed);
 				
-				pstmt.setString(1, artikelBean.getName());
-				pstmt.setInt(2, artikelBean.getPreis());
-				pstmt.setLong(3, (artikelBean.getKategorieId()));
-				pstmt.setBlob(4, inputStream);
-				pstmt.setString(5, artikelBean.getBildName());
-				pstmt.setLong(6, artikelBean.getId());
+			//Wenn nichts hochgeladen, wird das aktuelle Bild von der Datenbank geholt
+			InputStream original = null;
+			try(Connection con = ds.getConnection();
+					PreparedStatement pstmt = con.prepareStatement("SELECT bild FROM artikel WHERE id = ?")){
+				
+				pstmt.setLong(1, artikelBean.getId());
+				
+					try(ResultSet rs = pstmt.executeQuery()) {
+						if(rs.next()) {
+							original = rs.getBinaryStream("bild");
+						}
+					}
+			} catch (Exception ex) {
+				throw new ServletException(ex.getMessage());
+				}
+			
+			InputStreamReader reader = new InputStreamReader(inputStream);
+				if(!(reader.ready())){
+					inputStream = original;
+			}
+				
+			//Update bild, bildName in der Datenbank
+			try(Connection con = ds.getConnection();
+					PreparedStatement pstmt = con.prepareStatement("UPDATE artikel SET bild = ?, bildname = ? WHERE id = ?")){
+				
+				pstmt.setBlob(1, inputStream);
+				pstmt.setString(2, artikelBean.getBildName());
+				pstmt.setLong(3, artikelBean.getId());
 				pstmt.executeUpdate();
 				
 			} catch (Exception ex) {
 				throw new ServletException(ex.getMessage());
-				}
+			}
+		}
+		
+		//Update name, preis, idKategorie in der Datenbank
+		try(Connection con = ds.getConnection();
+				PreparedStatement pstmt = con.prepareStatement("UPDATE artikel SET name = ?, preis = ?, idKategorie = ? WHERE id = ?")){
+			
+			pstmt.setString(1, artikelBean.getName());
+			pstmt.setInt(2, artikelBean.getPreis());
+			pstmt.setLong(3, (artikelBean.getKategorieId()));
+			pstmt.setLong(4, artikelBean.getId());
+			pstmt.executeUpdate();
+			
+		} catch (Exception ex) {
+			throw new ServletException(ex.getMessage());
+		}
 
-			response.sendRedirect("admin/artikel_bearbeiten_erfolgreich.jsp");
+		response.sendRedirect("admin/artikel_bearbeiten_erfolgreich.jsp");
 	}
 	
 }
